f23829da44d6487fb2dec06ecd5c4a0437f28c32
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-04-27 02:58:51 +0530

    feat: rewrite wipe payload with `dd`, safer I/O, and better portability
    - Replaced `sg_dd` with standard `dd` for better availability and reliability.
    - Increased wipe coverage to 4MB per device to ensure destruction of superblocks, GPT backups, and bootloader data.
    - Used `conv=fsync` to flush write buffers per device, ensuring durability without a separate sync call.
    - Used status=none to suppress unnecessary output and improve parallel wipe clarity.
    - Switched to passing device as `$1` (not `$0`) in `xargs+bash -c` to avoid argument confusion.
    
    This version is faster, safer, and better suited for disk destruction across most Linux distributions.

--- wipe.sh
+++ wipe.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+#
+# wipe.sh - Emergency disk wipe payload for wrong8007
+# Author: 03C0
+#
+# Description:
+#   Wipes partition tables, signatures, and MBR data from all block devices
+#   matching /dev/sdX and /dev/nvmeXnY. Intended for rapid erasure.
+#
+# Tools used:
+#   - wipefs (to remove filesystem signatures)
+#   - sgdisk (to destroy GPT/MBR structures)
+#   - dd (to zero boot sectors)
 
-# title: Wipe Disks
-# description: Runs wipefs -af, sgdisk -Z and dd's the bootsector of every disk
+# Require root
+[[ $EUID -ne 0 ]] && exit 1
 
-find /dev -maxdepth 1 -type b -regextype "posix-extended" -regex '.*/(sd[a-z]$|nvme[0-9]n[0-9])$'|xargs -rn1 -P1 bash -c '
-[[ $(command -v wipefs) ]] && { wipefs -af ${0}; }
-[[ $(command -v sgdisk) ]] && { sgdisk -Z ${0}; }
-[[ $(command -v sg_dd) ]] && { sg_dd if=/dev/zero of=${0} bs=446 count=1; }
-'
-exit 0
+# Find disks and apply destructive operations
+find /dev -maxdepth 1 -type b -regextype "posix-extended" \
+  -regex '.*/(sd[a-z]$|nvme[0-9]n[0-9])$' | \
+xargs -rn1 -P4 bash -c '
+device="$1"
+echo "[*] Wiping $device"
+[[ $(command -v wipefs) ]] && wipefs -af "$device"
+[[ $(command -v sgdisk) ]] && sgdisk -Z "$device"
+dd if=/dev/zero of="$device" bs=1M count=4 conv=fsync status=none
+' _
