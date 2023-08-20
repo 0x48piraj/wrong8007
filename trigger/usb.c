4fd96480dc4229787b9e9932b416e038b9cd1120
Piyush Raj <piyush.189303055@muj.manipal.edu>
2023-08-20 20:58:52 +0530

    feat(trigger/usb): supersede parallel arrays with `VID:PID:EVENT` rule parser
    - Introduces `usb_devices=VID:PID:EVENT` format for user-friendly config
    - Replaces `usb_vids[]`, `usb_pids[]`, `usb_events[]` with structured `usb_dev_rule[]`
    - Parses rules once at init for fast runtime matching (no string comparisons)
    - Eliminates risk of array length mismatch bugs
    - Simplifies future extension (e.g. serial/class filters)
    
    Improves safety, clarity, and configurability of USB trigger logic.

--- trigger/usb.c
+++ trigger/usb.c
@@ -35,104 +35,122 @@
 
 #define MAX_USB_DEVICES 16
 
-/* Arrays for multi-device configuration */
-static int usb_vids[MAX_USB_DEVICES];
-static int usb_pids[MAX_USB_DEVICES];
-static char *usb_events[MAX_USB_DEVICES];
+// Device rule structure
+struct usb_dev_rule {
+    u16 vid;
+    u16 pid;
+    enum { USB_EVT_INSERT, USB_EVT_EJECT, USB_EVT_ANY } event;
+};
 
-/* Actual number of configured devices */
-static int usb_count_vid;
-static int usb_count_pid;
-static int usb_count_evt;
+// Storage for parsed rules
+static struct usb_dev_rule usb_rules[MAX_USB_DEVICES];
+static int usb_rule_count;
 
-/* Module params */
-module_param_array(usb_vids, int, &usb_count_vid, 0000);
-MODULE_PARM_DESC(usb_vids, "Array of USB Vendor IDs");
+// Device rules as strings: "VID:PID:EVENT"
+static char *usb_devices[MAX_USB_DEVICES];
+static int usb_devices_count;
+module_param_array(usb_devices, charp, &usb_devices_count, 0000);
+MODULE_PARM_DESC(usb_devices, "VID:PID:EVENT (EVENT=insert|eject|any)");
 
-module_param_array(usb_pids, int, &usb_count_pid, 0000);
-MODULE_PARM_DESC(usb_pids, "Array of USB Product IDs");
-
-module_param_array(usb_events, charp, &usb_count_evt, 0000);
-MODULE_PARM_DESC(usb_events, "Array of USB events: insert, eject, or any");
-
-// Declare the external exec_work from main module
+// External work from main module
 extern struct work_struct exec_work;
 
-/* Helper: whether the action matches the configured string */
-static bool match_event_type(const char *event, unsigned long action)
+// Parse module param usb_devices[] into structured rules
+static int parse_usb_devices(void)
 {
-    if (!event)
-        return false;
-    return (action == USB_DEVICE_ADD &&
-            (!strcmp(event, "insert") || !strcmp(event, "any"))) ||
-           (action == USB_DEVICE_REMOVE &&
-            (!strcmp(event, "eject") || !strcmp(event, "any")));
+    int i;
+    for (i = 0; i < usb_devices_count && i < MAX_USB_DEVICES; i++) {
+        unsigned int vid, pid;
+        char evt_str[16];
+        int n = sscanf(usb_devices[i], "%x:%x:%15s", &vid, &pid, evt_str);
+        if (n != 3) {
+            pr_err("wrong8007: Invalid USB rule '%s'\n", usb_devices[i]);
+            return -EINVAL;
+        }
+
+        usb_rules[usb_rule_count].vid = (u16)vid;
+        usb_rules[usb_rule_count].pid = (u16)pid;
+
+        if (!strcmp(evt_str, "insert"))
+            usb_rules[usb_rule_count].event = USB_EVT_INSERT;
+        else if (!strcmp(evt_str, "eject"))
+            usb_rules[usb_rule_count].event = USB_EVT_EJECT;
+        else if (!strcmp(evt_str, "any"))
+            usb_rules[usb_rule_count].event = USB_EVT_ANY;
+        else {
+            pr_err("wrong8007: Unknown event '%s' in rule '%s'\n",
+                   evt_str, usb_devices[i]);
+            return -EINVAL;
+        }
+
+        pr_info("wrong8007: rule[%d] VID=0x%04x PID=0x%04x EVENT=%s\n",
+                usb_rule_count, vid, pid, evt_str);
+
+        usb_rule_count++;
+    }
+    return 0;
 }
 
-static int usb_notifier_callback(struct notifier_block *self,
-                                 unsigned long action, void *dev)
+// Match a device/action against rules
+static bool match_rules(u16 vid, u16 pid, unsigned long action)
+{
+    int i;
+    for (i = 0; i < usb_rule_count; i++) {
+        if (vid == usb_rules[i].vid && pid == usb_rules[i].pid) {
+            if ((action == USB_DEVICE_ADD &&
+                 (usb_rules[i].event == USB_EVT_INSERT || usb_rules[i].event == USB_EVT_ANY)) ||
+                (action == USB_DEVICE_REMOVE &&
+                 (usb_rules[i].event == USB_EVT_EJECT || usb_rules[i].event == USB_EVT_ANY)))
+                return true;
+        }
+    }
+    return false;
+}
+
+// USB notifier callback
+static int usb_notifier_callback(struct notifier_block *self, unsigned long action, void *dev)
 {
     struct usb_device *udev = dev;
     u16 vid = le16_to_cpu(udev->descriptor.idVendor);
     u16 pid = le16_to_cpu(udev->descriptor.idProduct);
-    int i;
 
-    for (i = 0; i < min3(usb_count_vid, usb_count_pid, usb_count_evt); i++) {
-        if (vid == (u16)usb_vids[i] &&
-            pid == (u16)usb_pids[i] &&
-            match_event_type(usb_events[i], action)) {
-
-            pr_info("wrong8007: USB trigger[%d] matched "
-                    "(VID=0x%04x PID=0x%04x EVENT=%s). Scheduling exec.\n",
-                    i, vid, pid, usb_events[i]);
-            schedule_work(&exec_work);
-            break;
-        }
+    if (match_rules(vid, pid, action)) {
+        pr_info("wrong8007: USB trigger fired (VID=0x%04x PID=0x%04x)\n", vid, pid);
+        schedule_work(&exec_work);
     }
 
     return NOTIFY_OK;
 }
 
-// Declare notifier_block
 static struct notifier_block usb_nb = {
     .notifier_call = usb_notifier_callback,
 };
 
+// Init/exit
 static int trigger_usb_init(void)
 {
-    int i;
+    int ret = parse_usb_devices();
+    if (ret)
+        return ret;
 
-    if (usb_count_vid != usb_count_pid || usb_count_vid != usb_count_evt) {
-        pr_err("wrong8007: usb_vids, usb_pids, and usb_events must have same length\n");
-        return -EINVAL;
-    }
-    if (usb_count_vid <= 0) {
-        pr_err("wrong8007: No USB devices configured\n");
-        return -EINVAL;
-    }
-
-    for (i = 0; i < usb_count_vid; i++) {
-        if (!usb_events[i]) {
-            pr_err("wrong8007: usb_events[%d] is NULL\n", i);
-            return -EINVAL;
-        }
-        pr_info("wrong8007: Configured USB trigger[%d] "
-                "(VID=0x%04x PID=0x%04x EVENT=%s)\n",
-                i, usb_vids[i], usb_pids[i], usb_events[i]);
+    if (usb_rule_count == 0) {
+        pr_info("wrong8007: No USB rules configured, trigger disabled\n");
+        return 0;
     }
 
     usb_register_notify(&usb_nb);
-    pr_info("wrong8007: USB multi-device trigger initialized\n");
+    pr_info("wrong8007: USB trigger initialized (%d rules)\n", usb_rule_count);
     return 0;
 }
 
 static void trigger_usb_exit(void)
 {
-    usb_unregister_notify(&usb_nb);
+    if (usb_rule_count > 0)
+        usb_unregister_notify(&usb_nb);
     pr_info("wrong8007: USB trigger exited\n");
 }
 
-// Expose as a trigger plugin
+// Expose as trigger plugin
 struct wrong8007_trigger usb_trigger = {
     .name = "usb",
     .init = trigger_usb_init,
